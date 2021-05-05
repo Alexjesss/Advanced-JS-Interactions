a1283ba6ec5232ce2346622d8a92ba0c66484fe8
Alexandra <banica@9221@hotmail.be>
2021-05-05 10:53:15 +0200

    switch light/dark fixed, now fixing carousel

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function randomLetter(){
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
+
+let switchMode = document.getElementById('mode');
+let body = document.body;
+
+switchMode.addEventListener('click', () => {
+     body.classList.toggle('darkMode');
+})
