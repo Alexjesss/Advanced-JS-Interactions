e3e111087e131274a89494ef66e267b2c2ed14fc
Alexandra <banica@9221@hotmail.be>
2021-05-05 12:30:36 +0200

    working carousel but need to adjust for parallax, filled in collage with images

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
+let switchMode = document.getElementById('mode');
+let body = document.body;
+
+let slides = document.getElementsByClassName('carousel-image');
+let slidePosition = 0;
+let previous = document.getElementById('button-prev');
+let next = document.getElementById('button-next');
+let totalSlides = slides.length;
+console.log(slides);
+
+
 Array.from(document.querySelectorAll(".letter")).forEach(el => {
-   el.innerText = randomLetter();
+    el.innerText = randomLetter();
 });
 
-function randomLetter(){
+function randomLetter() {
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
 
-let switchMode = document.getElementById('mode');
-let body = document.body;
+function updateSlides(){
+    for (let slide of slides){
+        slide.classList.remove('carousel-image--visible');
+        slide.classList.add('carousel-image--hidden');
+    }
+    slides[slidePosition].classList.add('carousel-image--visible');
+}
+
+function movePrevSlide(){
+    if (slidePosition === 0){
+        slidePosition = totalSlides -1;
+    }
+    else {
+        slidePosition--;
+    }
+    updateSlides()
+}
+
+function moveNextSlide(){
+    if (slidePosition === totalSlides -1){
+        slidePosition = 0;
+    }
+    else {
+        slidePosition++;
+    }
+    updateSlides()
+}
 
 switchMode.addEventListener('click', () => {
-     body.classList.toggle('darkMode');
+    body.classList.toggle('darkMode');
 })
+
+previous.addEventListener('click', () => {
+    movePrevSlide();
+})
+
+next.addEventListener('click', () => {
+    moveNextSlide();
+})
+
