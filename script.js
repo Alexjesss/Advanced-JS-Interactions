2f1032409cffc5413b1fd8173e1f3bc28225adea
Alexandra <banica@9221@hotmail.be>
2021-05-07 12:23:14 +0200

    deleted carousel, doing it all over later, collage finally debugged styling issue, now finalizing collage

--- script.js
+++ script.js
@@ -3,19 +3,16 @@
 let switchMode = document.getElementById('mode');
 let body = document.body;
 
-// carousel //
+// carousel redoing it again...//
 
-let slides = document.getElementsByClassName('carousel-image');
-let slidePosition = 0;
-let previous = document.getElementById('button-prev');
-let next = document.getElementById('button-next');
-let totalSlides = slides.length;
+
+
 
 
 // collage //
 let collageImages = document.querySelectorAll(".image");
-let big = document.querySelector('.big');
-let imageEnlarged = document.getElementsByClassName('imageEnlarged');
+let large = document.querySelector('.large');
+let imageEnlarged = document.querySelector('.imageEnlarged');
 let collageDescription = document.getElementById('description');
 
 
@@ -28,57 +25,14 @@ function randomLetter() {
     return alphabet[Math.floor(Math.random() * alphabet.length)]
 }
 
-function updateSlides() {
-    for (let slide of slides) {
-        slide.classList.remove('carousel-image--visible');
-        slide.classList.add('carousel-image--hidden');
-    }
-    slides[slidePosition].classList.add('carousel-image--visible');
-}
-
-function movePrevSlide() {
-    if (slidePosition === 0) {
-        slidePosition = totalSlides - 1;
-    } else {
-        slidePosition--;
-    }
-    updateSlides()
-}
-
-function moveNextSlide() {
-    if (slidePosition === totalSlides - 1) {
-        slidePosition = 0;
-    } else {
-        slidePosition++;
-    }
-    updateSlides()
-}
 
 switchMode.addEventListener('click', () => {
     body.classList.toggle('darkMode');
 })
 
-previous.addEventListener('click', () => {
-    movePrevSlide();
-})
-
-next.addEventListener('click', () => {
-    moveNextSlide();
-})
-
-
-
-// window.onclick = function(e) {
-//     if (e.target == large) {
-//         large.style.display = "none";
-//     }
-// }
-
-
-
 collageImages.forEach(el => {
     el.addEventListener("click", (e) => {
-        big.style.display = 'block';
+        large.style.display = 'block';
         imageEnlarged.src = e.target.src;
         collageDescription.innerHTML = e.target.alt;
         console.log(e.target.alt);
@@ -86,3 +40,11 @@ collageImages.forEach(el => {
         console.log(imageEnlarged.src)
     });
 });
+
+// not finished yet! //
+
+window.onclick = function(event) {
+    if (event.target == document.body) {
+        large.style.display = "none";
+    }
+}
